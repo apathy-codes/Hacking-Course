6136d28178097de2c519b97adb90198478bb7d0a
apathy-codes <139901090+apathy-codes@users.noreply.github.com>
2023-07-18 12:53:35 -0400

    Update arp_spoofer.py
    Added message when discovery is disabled

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -30,7 +30,10 @@ def get_mac(ip):
     broadcast = scapy.Ether(dst=str("ff:ff:ff:ff:ff:ff"))
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    return answered_list[0][1].hwsrc
+    if answered_list:
+        return answered_list[0][1].hwsrc
+    else:
+        print("No response for ARP request, ICMP disabled?")
 
 sent_packet_count = 0
 try:
